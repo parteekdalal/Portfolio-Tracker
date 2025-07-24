fe0462e11dd47beb1649a19f4e38422a13c241e1
Parteek Dalal <parteekdalal9999@gmail.com>
2025-07-24 17:12:08 +0530

    create asset object

---
+++ asset.py
@@ -0,0 +1,58 @@
+import yfinance as yf
+import pandas as pd
+
+class Asset:
+    def __init__(self, symbol:str, quantity:float, buy_price:float):
+        self.symbol= symbol.upper()
+        self.quantity= quantity
+        self.buy_price= buy_price
+        self.invested_value= round(self.quantity * self.buy_price, 2)
+
+        # fetching data using yfinance
+        self.ticker = self.fetch_data()
+        if self.ticker is not None:
+            self.name = self.ticker.info.get("shortName", "Unknown")
+        else:
+            self.name = "Unknown"
+        print("Asset imported successfully")
+
+    """PROPERTIES"""
+    @property
+    def current_price(self):
+        if self.ticker:
+            return round(self.ticker.history(period="1d")["Close"].iloc[-1], 2)
+        else:
+            return 0.0
+        
+    @property
+    def current_value(self):
+        return round(self.quantity * self.current_price, 2)
+    
+    @property
+    def pnl(self):
+        return round(self.current_value - self.invested_value, 2)
+    
+    """METHODS"""
+    def fetch_data(self):
+        try:
+            print(f"Fetching data for {self.symbol}")
+            ticker =  yf.Ticker(self.symbol)
+            return ticker
+        except Exception as e:
+            print(f"Error fetching data")
+            return None
+
+    # this returns a data frame containing the required details    
+    def get_asset_df(self):
+        print("Gathering data")
+        data =  [[
+            self.name,
+            self.quantity,
+            self.buy_price,
+            self.current_price,
+            self.invested_value,
+            self.current_value,
+            self.pnl
+        ]]
+        columns = ["Name", "Quantity", "Average Buy Price", "Current Price", "Invested Value", "Current Value", "PnL"]
+        return pd.DataFrame(data=data, index=[self.symbol], columns=columns)
